d27fca808385159244ae4f6fe1ac0ef7a8b7b178
iampratik <iampratikraskar@gmail.com>
2026-09-11 23:03:25 +0530

    chore: initialize owedly backend

---
+++ src/test/java/com/owedly/OwedlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owedly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwedlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
